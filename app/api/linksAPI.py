6547bf057e64cffe3b6ad15e6f82c112d2fa948a
Pundalov Vlad <pundalov03@gmail.com>
2025-08-29 00:22:33 +0300

    Fixed some bugs: redirect became public

--- app/api/linksAPI.py
+++ app/api/linksAPI.py
@@ -1,15 +1,8 @@
-from urllib.parse import urlparse
-
 from fastapi import APIRouter, Depends, HTTPException, Request, Response, Form
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import select
-from starlette.responses import RedirectResponse
-
-from app.core.database import async_session_maker
-from app.models.link import Link
 from app.repositories.linkRepository import LinkRepository
 from app.repositories.userRepository import UserRepository
-from app.schemes.linkSchemes import SLinkCreate, SLink
+from app.schemes.linkSchemes import SLink
 from app.schemes.userSchemes import SUser
 
 router = APIRouter(prefix="/dashboard", tags=["Work with Links in Dashboard"])
@@ -23,6 +16,7 @@ async def get_links(request: Request, current_user: SUser = Depends(UserReposito
     links = await LinkRepository.get_links_by_user_id(current_user.id)
     total_links = len(links)
     total_clicks = sum(link.clicks_count for link in links)
+    domain = str(request.base_url.hostname)
 
     return templates.TemplateResponse("dashboard.html", {
         'request': request,
@@ -30,6 +24,7 @@ async def get_links(request: Request, current_user: SUser = Depends(UserReposito
         'links': links,
         'total_links': total_links,
         'total_clicks': total_clicks,
+        'domain': domain,
     })
 
 @router.get('/{link_id}', summary="Get link by id for current user")
@@ -46,26 +41,3 @@ async def create_link(original_url: str = Form(...),
 async def delete_link(link_id: int = Form(...),
                               current_user: SUser = Depends(UserRepository.require_auth)):
     return await LinkRepository.delete_link(link_id, current_user)
-
-@router.get('/r/{short_code}', summary='Redirect by short link')
-async def redirect_short_link(short_code: str,
-                              current_user: SUser = Depends(UserRepository.require_auth)):
-    async with async_session_maker() as session:
-        query = select(Link).where(Link.short_code == short_code, Link.user_id == current_user.id)
-        result = await session.execute(query)
-        link = result.scalar_one_or_none()
-
-        if not link:
-            raise HTTPException(status_code=404, detail='Link not found')
-
-        link.clicks_count += 1
-        await session.commit()
-
-        if not link.original_url.startswith(('http://', 'https://')):
-            link.original_url = f'https://{link.original_url}'
-
-        try:
-            urlparse(link.original_url)
-            return RedirectResponse(url=link.original_url, status_code=302)
-        except Exception:
-            raise HTTPException(status_code=400, detail="Invalid URL format")
--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+from urllib.parse import urlparse
+
 import uvicorn
 from fastapi import FastAPI, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy import select
 from starlette.staticfiles import StaticFiles
 
 from app.api import router
+from app.core import async_session_maker
+from app.models import Link
 from app.repositories import UserRepository
 from app.schemes import SUser
 
@@ -36,6 +41,28 @@ async def login(request: Request):
         return RedirectResponse(url="/dashboard", status_code=302)
     except HTTPException:
         return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get('/r/{short_code}', summary='Redirect by short link')
+async def redirect_short_link(short_code: str):
+    async with async_session_maker() as session:
+        query = select(Link).where(Link.short_code == short_code)
+        result = await session.execute(query)
+        link = result.scalar_one_or_none()
+
+        if not link:
+            raise HTTPException(status_code=404, detail='Link not found')
+
+        link.clicks_count += 1
+        await session.commit()
+
+        if not link.original_url.startswith(('http://', 'https://')):
+            link.original_url = f'https://{link.original_url}'
+
+        try:
+            urlparse(link.original_url)
+            return RedirectResponse(url=link.original_url, status_code=302)
+        except Exception:
+            raise HTTPException(status_code=400, detail="Invalid URL format")
 
 @app.exception_handler(HTTPException)
 async def auth_exception_handler(request: Request, exc: HTTPException):
